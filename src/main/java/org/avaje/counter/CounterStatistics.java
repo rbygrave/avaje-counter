a9fc72d314f8f0d7c8ca4482d0c329507f01b8d5
Rob Bygrave <robin.bygrave@gmail.com>
2014-01-10 00:11:55 +1300

    initial add

---
+++ src/main/java/org/avaje/counter/CounterStatistics.java
@@ -0,0 +1,68 @@
+package org.avaje.counter;
+
+/**
+ * Snapshot of the current statistics for a Counter or TimeCounter.
+ */
+public class CounterStatistics {
+
+  protected final long startTime;
+  
+  protected final long count;
+  
+  protected final long total;
+
+  protected final long max;
+
+  /**
+   * Construct for Counter which doesn't collect time or high water mark.
+   */
+  public CounterStatistics(long collectionStart, long count) {
+    this.startTime = collectionStart;
+    this.count = count;
+    this.total = 0;
+    this.max = 0;
+  }
+  
+  /**
+   * Construct for TimeCounter.
+   */
+  public CounterStatistics(long collectionStart, long count, long total, long max) {
+    this.startTime = collectionStart;
+    this.count = count;
+    this.total = total;
+    this.max = max;
+  }
+
+  public String toString() {
+    return "count:"+count+" total:"+total+" max:"+max;
+  }
+  
+  /**
+   * Return the time the counter started statistics collection.
+   */
+  public long getStartTime() {
+    return startTime;
+  }
+
+  /**
+   * Return the count of values collected.
+   */
+  public long getCount() {
+    return count;
+  }
+
+  /**
+   * Return the total of all the values.
+   */
+  public long getTotal() {
+    return total;
+  }
+
+  /**
+   * Return the Max value collected.
+   */
+  public long getMax() {
+    return max;
+  }
+
+}
